4a4d56320ec06c472c42c5b5a56b0a54708fff53
Peter Tran <tranpeter1999@gmail.com>
2017-11-13 16:20:53 -0800

    Add files via upload

--- roblox dev malice clientside cd handling.lua
+++ roblox dev malice clientside cd handling.lua
@@ -22,14 +22,48 @@ local function visualize()
 		
 		local currentCD = charCDsFolder:FindFirstChild(cdFolder.AbilityName.Value)
 		
-		if currentCD == nil then
-			v.CDBarFrame.Frame.Size = UDim2.new(0,0,0,0)
-		else
-			local perc = currentCD.Value/cdFolder.AbilityCD.Value
-			
+		if currentCD then
+			local perc = (currentCD.Value-10)/cdFolder.AbilityCD.Value
+			v.CDBarFrame.Bar:TweenSize(UDim2.new(1,0,perc,0),"Out","Linear",0.1,true)
+			cdFrame.CDBarFrame.Bar.Transparency = 0.5
 		end
 	end
 end
+
+charCDsFolder.ChildAdded:connect(function(child)
+	if characterFolder.cds:FindFirstChild(child.Name) then
+		local cdFrame = nil
+		
+		for _,v in pairs(script.Parent.MainUI.Cooldowns:GetChildren())do
+			if v.DescriptionFrame.AbilityNameTextLabel.Text == child.Name then
+				cdFrame = v
+			end
+		end
+		
+		cdFrame.CDBarFrame.Bar.Size = UDim2.new(1,0,1,0)
+		cdFrame.CDBarFrame.Bar.Transparency = 0.5
+	end
+end)
+
+charCDsFolder.ChildRemoved:connect(function(child)
+	if characterFolder.cds:FindFirstChild(child.Name) then
+		local cdFrame = nil
+		
+		for _,v in pairs(script.Parent.MainUI.Cooldowns:GetChildren())do
+			if v.DescriptionFrame.AbilityNameTextLabel.Text == child.Name then
+				cdFrame = v
+			end
+		end
+		
+		cdFrame.CDBarFrame.Bar:TweenSize = (UDim2.new(1,0,1,0),"Out","Quad",0.25,false)
+		cdFrame.CDBarFrame.Bar.Transparency = 0.5
+		delay(0.25,function()
+			while charCDsFolder:FindFirstChild(child.Name) == nil and cdFrame.CDBarFrame.Bar.Transparency < 1 do
+				cdFrame.CDBarFrame.Bar.Transparency = 0.5 + (i/10)*0.5
+			end
+		end)
+	end
+end)
 
 game.Workspace.UpdateCDs.Changed:connect(function()
 	spawn(visualize)
